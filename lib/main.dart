6ecae5d46fcc97f127ac607cf128d4af7dfdf247
Mehrdad08 <moradi.mehrdad001@gmail.com>
2022-08-24 20:28:52 +0430

    1- Add keyBoard_buttons.dart 2- Add some colors in const_color.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,37 @@
+import 'package:calculator_task/constants/const_color.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(Application());
 }
 
-
 class Application extends StatelessWidget {
   const Application({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: SafeArea(
+          child: Column(
+            children: [
+              Expanded(
+                flex: 3,
+                child: Container(
+                  color: backgroundGreyDark,
+                ),
+              ),
+              Expanded(
+                flex: 7,
+                child: Container(
+                  color: backgroundGrey,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
